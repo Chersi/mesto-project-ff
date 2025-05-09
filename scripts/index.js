ceffc344495b615bb2433289bd173769c75ecf3c
Chersi <cherepoks@mail.ru>
2025-05-09 09:42:29 +0300

    firstCommit

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,30 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+function addCard(cardData, deleteCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    cardElement.querySelector('.card__image').src = cardData.link;
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', () => {
+        deleteCard(cardElement);
+    });
+        
+    document.querySelector('.places__list').append(cardElement);
+};
+
+function deleteCard(card) {
+    card.remove();
+}
+
+function addAllCards(cardsArray) {
+    cardsArray.forEach((cardData) => {
+        addCard(cardData, deleteCard);
+    });
+}
+
+addAllCards(initialCards);
